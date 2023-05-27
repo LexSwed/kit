21180bcfaec7c06157cdabe07eeb2383f2dadb4b
LexSwed <lexswed@gmail.com>
2023-05-27 16:01:49 +0200

    add CSS custom Highlights

--- packages/ui/src/lib/theme-provider/theme-provider.tsx
+++ packages/ui/src/lib/theme-provider/theme-provider.tsx
@@ -23,36 +23,52 @@ import styles from './theme-provider.module.css';
 type Props = {
   children?: ReactNode;
   theme?: Theme;
+  /**
+   * Apply all CSS variables to global window. Allows accessing CSS variables from ::overlay, :root, etc.
+   * Pass `false` if polluting global :root is not desirable, f.e. in client extensions.
+   * @default true
+   */
+  globalCss?: boolean;
 } & ComponentProps<'div'>;
 
-const ThemeProvider = forwardRef<HTMLDivElement, Props>(({ theme = {}, className, children, ...props }, propRef) => {
-  const rootRef = useFxtrotRootRef();
-  const ref = useRef<HTMLDivElement>(null);
-  const [direction, directionRef] = useDirection();
-  const refs = useForkRef<HTMLElement>(ref, directionRef, propRef);
-  // slice :r0: -> r0
-  const themeClassName = `fxtrot-ui-${useId().slice(1, -1)}`;
-  const css = createThemeCssText(themeClassName, theme);
+const ThemeProvider = forwardRef<HTMLDivElement, Props>(
+  ({ theme = {}, globalCss = true, className, children, ...props }, propRef) => {
+    const rootRef = useFxtrotRootRef();
+    const ref = useRef<HTMLDivElement>(null);
+    const [direction, directionRef] = useDirection();
+    const refs = useForkRef<HTMLElement>(ref, directionRef, propRef);
 
-  return (
-    <rootRefContext.Provider value={rootRef.current ? rootRef : ref}>
-      <DirectionProvider dir={direction}>
-        <TooltipProvider delayDuration={400}>
-          <>
-            <style dangerouslySetInnerHTML={{ __html: css }} />
-            <div {...props} className={clsx(styles['fxtrot-ui-theme'], themeClassName, className)} ref={refs}>
-              {children}
-            </div>
-          </>
-        </TooltipProvider>
-      </DirectionProvider>
-    </rootRefContext.Provider>
-  );
-});
+    // slice :r0: -> r0
+    const themeClassName = `fxtrot-ui-${useId().slice(1, -1)}`;
+    let css: string;
+    if (globalCss) {
+      css = createThemeCssText('*, ::selection, ::before, ::after', theme);
+      css += `@supports selector(::backdrop) {${createThemeCssText('::backdrop', theme)}}`;
+      css += `@supports selector(::highlight(editor)) {${createThemeCssText('::highlight(editor)', theme)}}`;
+    } else {
+      css = createThemeCssText(`.${themeClassName}`, theme);
+    }
 
-function createThemeCssText(className: string, theme: Theme) {
+    return (
+      <rootRefContext.Provider value={rootRef.current ? rootRef : ref}>
+        <DirectionProvider dir={direction}>
+          <TooltipProvider delayDuration={400}>
+            <>
+              <style dangerouslySetInnerHTML={{ __html: css }} />
+              <div {...props} className={clsx(styles['fxtrot-ui-theme'], themeClassName, className)} ref={refs}>
+                {children}
+              </div>
+            </>
+          </TooltipProvider>
+        </DirectionProvider>
+      </rootRefContext.Provider>
+    );
+  }
+);
+
+function createThemeCssText(selector: string, theme: Theme) {
   const fullTheme = mergeTheme(theme);
-  const css = `.${className} {${createThemeVariables(fullTheme)
+  const css = `${selector} {${createThemeVariables(fullTheme)
     .map((entry) => entry.join(':'))
     .join(';')};}`.trim();
   return css;
